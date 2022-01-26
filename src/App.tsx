81352f965531304b873c05deb44183c4fbf6a4e2
git@pierrefhapp.dev <git@pierrefhapp.dev>
2022-01-26 20:51:19 +0100

    initial state. testing useState and setState :]

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [number, setNumber] = useState(5);
+
+  // const [numOrString, setNumberOrString] = useState<string | number>(5);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +14,13 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <p>Number is {number}</p>
+        <button onClick={() => setNumber(number + 1)}>
+          Increase Value on Click{" "}
+        </button>
+        <button onClick={() => setNumber(number - 1)}>
+          Decrease Value on Click
+        </button>
         <a
           className="App-link"
           href="https://reactjs.org"
